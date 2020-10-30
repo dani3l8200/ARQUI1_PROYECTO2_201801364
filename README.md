a30eddaa9a8cff7881aec490f077957bc04a1955
Juan Daniel <dani3l25948@gmail.com>
2020-10-30 10:17:06 -0600

    inicializacion del proyecto 2

---
+++ README.md
@@ -0,0 +1,17 @@
+# Proyecto 2
+
+## Table of contents
+- [Proyecto 2](#proyecto-2)
+  - [Table of contents](#table-of-contents)
+  - [Informacion General](#informacion-general)
+  - [Technologies](#technologies)
+
+## Informacion General
+Practica 4: consiste en la realizacion de un juego de brick ball clasico realizado en assembly con la opcion de poder generar reportes
+por ordenamientos burbuja, quicksort y marge sort, donde se generan graficas de los tops de los jugadores
+
+
+## Technologies
+Esta practica fue realizada con las siguientes herramientas:
+* MASM
+* DosBox
---
+++ files.asm
@@ -0,0 +1,70 @@
+;GetPahtFile sirve para introducir el directorio donde se encuentre algun archivo 
+GetPathFile MACRO array
+	            LOCAL      getCadena, endCadena, RemoveSpace
+				; setea el registro SI con 0
+	            MOV        SI, 0
+	getCadena:  
+				;lee la consola para introducir la ubicacion del archivo
+	            ReadKeyPad
+				;compara si hay un enter que seria un retorno de carro
+	            CMP        AL, 0dh
+	            JE         endCadena
+				;esto es por si se metio mal alguna letra poder borrar la de atras
+	            CMP        AL, 08h
+	            JE         RemoveSpace
+				;se mueve al path el contenido 
+	            MOV        array[SI], AL
+				;incrementa SI
+	            INC        SI
+				;se vuelve a repetir el ciclo
+	            JMP        getCadena
+	RemoveSpace:
+				;mueve un fin de cadena a AL
+	            MOV        AL, 24h
+				;decrementa SI SI--
+	            DEC        SI
+				;mueve el caracter al path una casilla anterior
+	            MOV        array[SI], AL
+				;regresa al getCadena para seguir anadiendo al path
+	            JMP        getCadena
+	endCadena:  
+				;termina de ingresar el path 
+	            MOV        array[SI], 00h
+ENDM
+
+;openFile macro para abrir el archivo 
+openFile MACRO file, handler
+	         MOV AH, 3dh    	;abre el archivo
+	         MOV AL, 10b   	;Acceso lectura/escritura
+	         lea DX, file   	;realiza una lectura con la informacion
+	         int 21h
+	         MOV handler, AX	;transfiere la informacion al handler
+	         jc  ErrorOpen  	;si hay carry lanzara un error ya que no pudo abrir el archivo con exito
+ENDM
+
+;createFile macro para crear archivos 
+createFile MACRO file, handler
+	           MOV AH, 3ch    	;crea el archivo
+	           MOV CX, 00h    	;mueve al inicio del archivo
+	           lea DX, file   	;realiza una lectura con la informacion
+	           int 21h
+	           MOV handler, AX	;transfiere la informacion al handler
+	           jc  ErrorCreate	;si hay carry lanzara un error ya que no se creo con exito el archivo
+ENDM
+
+;WriteFile macro para escribir el archivo 
+writeFile MACRO array, handler, numBytes
+	          PUSH CX
+	          PUSH DX
+
+	          MOV  AH, 40h
+	          MOV  BX, handler
+	          MOV  CX, numBytes
+	          lea  DX, array
+	          int  21h
+	          jc   ErrorWrite
+
+	          POP  DX
+	          POP  CX
+ENDM
+
